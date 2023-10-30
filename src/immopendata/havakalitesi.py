27a6d3094d7f70dbb7dbd87594613a77912d4d2c
Deniz G <53919703+gokdumano@users.noreply.github.com>
2023-10-31 00:33:15 +0300

    Add files via upload

--- src/immopendata/havakalitesi.py
+++ src/immopendata/havakalitesi.py
@@ -1,16 +1,22 @@
-from dtypes import Station, Concentration, AQI
-
 from datetime import datetime
 import requests, re
 
-def GetAQIStations() -> list[Station]:
-    '''Bu web servis ile Hava Kalitesi istasyonlarının bilgileri paylaşılmıştır
+def GetAQIStations() -> list[dict]:
+    """Information of Air Quality stations is shared with this web service.
 
-    Returns
-    -------
-    stations
-        Portal üzerinde kayıtlı Hava Kalitesi istasyonlarını içeren bir liste
-    '''
+    Id (int): Contains StationId information. It is a unique field.
+    
+    Name (str): Contains the station name information.
+    
+    Address (str): Contains station address information.
+    
+    Latitude (float): Contains the Latitude of the station.
+    
+    Longitude (float): Contains the Longitude of the station.
+    
+    Returns:
+        dict: Returns the list of stationId, Name, Address and Location.
+    """
     with requests.Session() as s:
         url = 'https://api.ibb.gov.tr/havakalitesi/OpenDataPortalHandler/GetAQIStations'
         r = s.get(url, headers={'Accept-Encoding': 'gzip, deflate'})
@@ -28,36 +34,29 @@ def GetAQIStations() -> list[Station]:
                 'Latitude': float(matches.group('Lat')),
                 'Longitude': float(matches.group('Lng'))
                 })
-            stations.append(Station(**station))
+            stations.append(station)
             
         return stations
     
-def GetAQIByStationId(stationId: str, startDate: datetime, endDate: datetime) -> tuple[list[Concentration], list[AQI]]:
-    '''Bu web servis ile Id’si girilen istasyonun başlangıç ve bitiş tarihine göre Konsantrasyon (Concentration) ve 
-    Hava Kalitesi Index (AQI) bilgileri paylaşılmaktadır.
-
-    Parameters
-    ----------
-    stationId : str
-        Ölçüm bilgileri getirilecek istasyonun Id’sidir. ('GetAQIStations' metodu ile ulaşılabilir.)
+def GetAQIByStationId(stationId: str, startDate: datetime, endDate: datetime) -> tuple[list[dict], list[dict]]:
+    """With this web service, Concentration and Air Quality Index (AQI)
+    information is shared according to the start and end dates of the station
+    whose ID is entered.
+    
+    Parameters:
+        stationId (int): It is the Unique Id of the station whose measurement information will be retrieved.
         
-    startDate : datetime
-        Belirtilen tarih ölçümlerin başlangıç tarihi olarak belirlenir.
+        startDate (datetime): The specified date is determined as the startdate of the measurements.
         
-    endDate : datetime
-        Belirtilen tarih ölçümlerin  bitiş tarihi (dahil) olarak belirlenir.
-
-    Returns
-    -------
-    C: list[Concentration]
-        Ölçüm tarihi bilgisi ve istasyonun analizörden ölçülen PM10, SO2, O3, NO2 ve CO ham verilerinin değerini içerir.
+        endDate (datetime): The specified date is determined as the end date of the measurements.
+ 
+    Returns:
+        list[dict]: PM10, SO2, O3, NO2 and CO measured from the Station's Analyzer contains the value of the data.
         
-    A: list[AQI]
-        Ölçüm tarihi bilgisi ve istasyonun analizörden ölçülen PM10, SO2, O3, NO2 ve CO ham verilerinin Hava Kalitesi 
-        Index'i hesaplandıktan sonraki değerlerini ve o saat için HKI değerini, Kirletici parametreyi, durumu ve durumun 
-        rengini içerir.
-
-    '''
+        list[dict]: The values of PM10, SO2, O3, NO2 and CO raw data measured from the Analyzer of the station after
+        calculating the AirQuality Index and the HKI value for that hour, the Pollutant parameter,the status
+        and the status includes color.
+    """
     with requests.Session() as s:
         url = 'https://api.ibb.gov.tr/havakalitesi/OpenDataPortalHandler/GetAQIByStationId'
         params = {
@@ -71,12 +70,12 @@ def GetAQIByStationId(stationId: str, startDate: datetime, endDate: datetime) ->
         C, A = [], []
         for record in r.json():
             ReadTime = record.pop('ReadTime')
-            ReadTime = datetime.fromisoformat(ReadTime)
+            ReadTime = datetime.fromisoformat(ReadTime).isoformat()
             if c := record.get('Concentration'):
                 c |= {'ReadTime': ReadTime}
-                C.append(Concentration(**c))
+                C.append(c)
             if a := record.get('AQI'):
                 a |= {'ReadTime': ReadTime}
-                A.append(AQI(**a))
+                A.append(a)
 
         return C, A
--- src/immopendata/ispark.py
+++ src/immopendata/ispark.py
@@ -1,44 +1,150 @@
-from dtypes import Detail, Park
-
 from datetime import datetime
 import requests
 
-def GetAllParks() -> list[Park]:
+ParkColumns = {
+    'parkID'       : 'Id',
+    'parkName'     : 'Name',
+    'lat'          : 'Latitude',
+    'lng'          : 'Longitude',
+    'capacity'     : 'Capacity',
+    'emptyCapacity': 'EmptyCapacity',
+    'workHours'    : 'WorkHours',
+    'parkType'     : 'Type',
+    'freeTime'     : 'FreeTime',
+    'district'     : 'District',
+    'isOpen'       : 'IsOpen'
+    }
+
+DetailColumns = {
+    'parkID'       : 'Id',
+    'parkName'     : 'Name',
+    'lat'          : 'Latitude',
+    'lng'          : 'Longitude',
+    'capacity'     : 'Capacity',
+    'emptyCapacity': 'EmptyCapacity',
+    'workHours'    : 'WorkHours',
+    'parkType'     : 'Type',
+    'freeTime'     : 'FreeTime',
+    'district'     : 'District',
+    'locationName' : 'LocationName',
+    'updateDate'   : 'UpdateTime',
+    'monthlyFee'   : 'MonthlyFee',
+    'tariff'       : 'Tariff',
+    'address'      : 'Address',
+    'areaPolygon'  : 'AreaPolygon'
+    }
+
+def GetAllParks() -> list[dict]:
+    """This method will return the information of all Ispark car parks.
+    The returning list will have dictionary with the following parameters
+
+    Capacity (int): The total capacity of the park.
+    
+    District (str): District where the park is located.
+    
+    EmptyCapacity (int): Empty capacity of the park.
+    
+    FreeTime (int): Free parking time (minute).
+    
+    Id (int): Contains Park Id information. It is a unique field.
+    
+    IsOpen (bool): Is the park open.
+    
+    Latitude (float): Latitude information of the park.
+    
+    Longitude (float): Longitude information of the park.
+    
+    Name (str): Contains park name information.
+    
+    Type (str): Type of park (on road, open, closed).
+    
+    WorkHours (str): Working hours of the park.
+    
+    Returns:
+        list[dict]: The information of all Ispark car parks
+    """
     with requests.Session() as s:        
         url = 'https://api.ibb.gov.tr/ispark/Park'
-        r = s.get(url, headers={'Accept-Encoding':'gzip,deflate'})
+        r = s.get(url, headers={'Accept-Encoding': 'gzip,deflate'})
         r.raise_for_status()
 
         parks = []
         for park in r.json():
-            _ = {newName:park[oldName] for oldName, newName in Park.Mapper.items()}
-            _.update({'Latitude':float(_['Latitude']), 'Longitude':float(_['Longitude']), 'IsOpen': _['IsOpen'] != 0})
-            parks.append(Park(**_))
+            park = {newName: park[oldName] for oldName, newName in ParkColumns.items()}
+            park.update({
+                'Latitude': float(park['Latitude']),
+                'Longitude': float(park['Longitude']),
+                'IsOpen': park['IsOpen'] != 0
+                })
+            parks.append(park)
             
         return parks
     
-def GetParkDetail(parkId:int) -> Detail:
+def GetParkDetail(parkId: int) -> dict:
+    """With this web service, detailed information of any Ispark car park
+    can be accessed with given parkId
+
+    Id (int): Contains Park Id information. It is a unique field.
+    
+    Name (str): Contains park name information.
+    
+    Latitude (float): Contains park latitude information.
+    
+    Longitude (float): Contains park longitude information.
+    
+    Capacity (int): The total capacity of the park.
+    
+    EmptyCapacity (int): The empty capacity of the park.
+    
+    WorkHours (str): Park opening hours.
+    
+    Type (str): Type of park (on road, open, closed).
+    
+    FreeTime (int): Free parking time (minute).
+    
+    District (str): District where the park is located.
+    
+    LocationName (str): Contains location name information.
+    
+    UpdateTime (str): Update date and time of information
+    
+    MonthlyFee (float): Monthly Subscription Fee.
+    
+    Tariff (dict[str, float]): List of Tariff Information object with Tariff(string) and Price(float) key-value pairs
+    
+    Address (str): Parking lot address.
+    
+    AreaPolygon (str): Parking polygon information.
+    
+    Parameters:
+        parkId (int): The desired park id.
+ 
+    Returns:
+        dict: Detailed information about desired car park.
+    """
     with requests.Session() as s:        
         url = 'https://api.ibb.gov.tr/ispark/ParkDetay'
         details = []
 
-        r = s.get(url, params={'id':parkId}, headers={'Accept-Encoding':'gzip,deflate'})
+        r = s.get(url, params={'id': parkId}, headers={'Accept-Encoding': 'gzip,deflate'})
         r.raise_for_status()
 
         detail, = r.json()
-        _ = {newName:detail[oldName] for oldName, newName in Detail.Mapper.items()}
+        detail = {newName: detail[oldName] for oldName, newName in DetailColumns.items()}
 
         tariff = {}
-        for t in _['Tariff'].split(';'):
-            interval, price = t.replace(',','.').split(':')
+        for t in detail['Tariff'].split(';'):
+            interval, price = t.replace(',', '.').split(':')
             tariff[interval.rstrip()] = float(price)
 
-        if updateTime := _['UpdateTime']:
-            updateTime = datetime.strptime(updateTime, '%d.%m.%Y %H:%M:%S')
+        if updateTime := detail['UpdateTime']:
+            updateTime = datetime.strptime(updateTime, '%d.%m.%Y %H:%M:%S').isoformat()
 
-        _.update({'Tariff':tariff, 'Latitude':float(_['Latitude']), 'Longitude':float(_['Longitude']), 'UpdateTime':updateTime})
-        detail = Detail(**_)
+        detail.update({
+            'Tariff': tariff,
+            'Latitude': float(detail['Latitude']),
+            'Longitude': float(detail['Longitude']),
+            'UpdateTime': updateTime
+            })
+        
         return detail
-
-
-p = GetAllParks()
